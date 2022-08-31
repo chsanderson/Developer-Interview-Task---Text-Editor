27da815b81d51ba11f703d35d687ff367545b9ce
ChSan <chsanderson@live.co.uk>
2022-08-31 16:39:03 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] linesOfText = new string[4];
+linesOfText[0] = "Welcome to the timewarp of programs!";
+linesOfText[1] = "Applications like this were used in in the 1980s.";
+Console.WriteLine("Hello, World!");
