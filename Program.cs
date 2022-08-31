c9a8f6300f34c75067bc35d96e60a9bc76d5a21a
ChSan <chsanderson@live.co.uk>
2022-08-31 17:08:35 +0100

    Requirement 1

--- Program.cs
+++ Program.cs
@@ -2,4 +2,25 @@
 string[] linesOfText = new string[4];
 linesOfText[0] = "Welcome to the timewarp of programs!";
 linesOfText[1] = "Applications like this were used in in the 1980s.";
-Console.WriteLine("Hello, World!");
+linesOfText[2] = "I can't wait for User Interfaces to be invented.";
+linesOfText[3] = "Then I can do much more complicated things";
+bool exit = false;
+string userResponse = Console.ReadLine();
+switch (userResponse)
+{
+    case ("I"):
+        for (int i = 0; i < linesOfText.Length; i++)
+        {
+            Console.WriteLine(linesOfText[i]);
+        }
+        break;
+    case ("L"):
+        for (int i = 0; i < linesOfText.Length; i++)
+        {
+            Console.WriteLine(linesOfText[i]);
+        }
+        break;
+    default:
+        break;
+}
+Console.ReadLine();
