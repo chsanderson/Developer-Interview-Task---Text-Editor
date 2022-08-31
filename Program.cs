28dd56176bb02a6547bb94928ba770c134804dae
ChSan <chsanderson@live.co.uk>
2022-08-31 19:58:06 +0100

    Update Requirement 2

--- Program.cs
+++ Program.cs
@@ -12,28 +12,59 @@ try
     do
     {
         Console.WriteLine("Enter 'I' or 'L' to view lines of text");
+        Console.WriteLine("Enter 'A' or 'a' along with some text to add to the lines of text");
         Console.WriteLine("Waiting for Response:");
         userResponse = Console.ReadLine();
         switch (userResponse.Substring(0, 1))
         {
             case ("I"):
-                for (int i = 0; i < linesOfText.Length; i++)
-                {
-                    Console.WriteLine(linesOfText[i]);
+                                for (int i = 0; i < linesOfText.Length; i++)
+                                {
+                                    Console.WriteLine(linesOfText[i]);
+                                }
+                                break;
+                            case ("L"):
+                                for (int i = 0; i < linesOfText.Length; i++)
+                                {
+                                    Console.WriteLine(linesOfText[i]);
+                                }
+                                break;
+                            case ("A"):
+                                addNewLine = linesOfText.Length + 1;
+                                temp = new string[addNewLine];
+                                for (int i = 0; i < (temp.Length - 1); i++)
+                                {
+                                    temp[i] = linesOfText[i];
+                                }
+                                temp[(addNewLine - 1)] = userResponse.Substring(1).Trim();
+                                linesOfText = new string[addNewLine];
+                                linesOfText = temp;
+                                /*for (int i = 0; i < addNewLine; i++)
+                                {
+                                    Console.WriteLine(linesOfText[i]);
+                                }*/ //This for loop displays the lines of text showing that the new line has been added
+                                break;
+                            case ("a"):
+                                addNewLine = linesOfText.Length + 1;
+                                temp = new string[addNewLine];
+                                for (int i = 0; i < (temp.Length - 1); i++)
+                                {
+                                    temp[i] = linesOfText[i];
+                                }
+                                temp[(addNewLine - 1)] = userResponse.Substring(1).Trim();
+                                linesOfText = new string[addNewLine];
+                                linesOfText = temp;
+                                /*for (int i = 0; i < addNewLine; i++)
+                                {
+                                    Console.WriteLine(linesOfText[i]);
+                                }*/ //This for loop displays the lines of text showing that the new line has been added
+                                break;
+                            default:
+                                break;
+                        }
+                    } while (userResponse.Length > 0);
                 }
-                break;
-            case ("L"):
-                for (int i = 0; i < linesOfText.Length; i++)
+                catch (Exception ex)
                 {
-                    Console.WriteLine(linesOfText[i]);
+                    //Console.WriteLine(ex.ToString());
                 }
-                break;
-            default:
-                break;
-        }
-    } while (userResponse.Length > 0);
-}
-catch (Exception ex)
-{
-    //Console.WriteLine(ex.ToString());
-}
